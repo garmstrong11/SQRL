400199ab6dcfe3e9e529bc7c3d7896313bdc5949
GaryHome <gary.m.armstrong@gmail.com>
2013-01-12 17:42:54 -0600

    LookupController Route changes

--- SQRL.WebUI/App_Start/RouteConfig.cs
+++ SQRL.WebUI/App_Start/RouteConfig.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.RegularExpressions;
 using System.Web;
 using System.Web.Http;
 using System.Web.Mvc;
@@ -18,6 +19,13 @@ namespace SQRL.WebUI
 					name: "DefaultApi",
 					routeTemplate: "api/{controller}/{id}",
 					defaults: new { id = RouteParameter.Optional }
+			);
+
+			routes.MapRoute(
+				name: "Sqrl"
+				, url: "{id}"
+				, defaults: new { controller = "Lookup", action = "Index"}
+				, constraints: new { id = @"[A-Z]{5}" }
 			);
 
 			routes.MapRoute(
---
+++ SQRL.WebUI/Controllers/LookupController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web.Mvc;
+
+namespace SQRL.WebUI.Controllers
+{
+	public class LookupController : Controller
+	{
+		//
+		// GET: /Lookup/
+
+		public ActionResult Index(string id)
+		{
+			var siteDict = new Dictionary<string, string>(StringComparer.Ordinal) {
+				{"ABCDE", "http://www.amazon.com"},
+				{"AbCDE", "http://www.integracolor.com"},
+				{"BC9EF", "http://www.google.com"},
+				{"SQRLE", "http://localhost:50082/Home/Index"}
+			};
+
+			if (siteDict.ContainsKey(id)) {
+				return RedirectPermanent(siteDict[id]);
+			}
+
+			return HttpNotFound();
+		}
+	}
+}

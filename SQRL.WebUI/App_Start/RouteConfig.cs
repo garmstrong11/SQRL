30dc24ea1ca847e72d54621618737ef5a8480776
GaryHome <gary.m.armstrong@gmail.com>
2013-01-20 10:43:12 -0600

    Amended RouteConfig

--- SQRL.WebUI/App_Start/RouteConfig.cs
+++ SQRL.WebUI/App_Start/RouteConfig.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text.RegularExpressions;
-using System.Web;
-using System.Web.Http;
+﻿using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Routing;
 
@@ -16,23 +11,23 @@ namespace SQRL.WebUI
 			routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
 			routes.MapHttpRoute(
-					name: "DefaultApi",
-					routeTemplate: "api/{controller}/{id}",
-					defaults: new { id = RouteParameter.Optional }
-			);
+				name: "DefaultApi",
+				routeTemplate: "api/{controller}/{id}",
+				defaults: new {id = RouteParameter.Optional}
+				);
 
 			routes.MapRoute(
 				name: "Sqrl"
 				, url: "{id}"
-				, defaults: new { controller = "Lookup", action = "Index"}
-				, constraints: new { id = @"[A-Z]{5}" }
-			);
+				, defaults: new {controller = "Lookup", action = "Index"}
+				, constraints: new {id = @"[A-Z0-9]{5}"}
+				);
 
 			routes.MapRoute(
-					name: "Default",
-					url: "{controller}/{action}/{id}",
-					defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-			);
+				name: "Default",
+				url: "{controller}/{action}/{id}",
+				defaults: new {controller = "Home", action = "Index", id = UrlParameter.Optional}
+				);
 		}
 	}
 }
